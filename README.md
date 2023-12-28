135397f873afbf27bd939a62ac97c788601abb3e
苏向夜 <fu050409@163.com>
2023-12-28 13:19:20 +0800

    :sparkles: 实现登录及登录验证窗口

---
+++ README.md
@@ -0,0 +1,17 @@
+<div align="center">
+    <img src="./static/Quantumix.png" alt="Quantumix" width="200" height="200"></img>
+</div>
+<div align="center">
+
+# Quantumix
+
+</div>
+启动Quantumix：
+
+```sh
+python manage.py runserver 0.0.0.0:80
+```
+
+## 版权声明
+
+此项目以 Apache-2.0 协议开源, 使用代码时, 请注意遵照开源协议.
---
+++ src/components/Login/Description.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
+
+<template>
+    <Card style="width: 100%; height: 100%;" class="flex align-items-center justify-content-center">
+        <template #title> 浊莲 </template>
+        <template #subtitle> Quantumix </template>
+        <template #content>
+            <p class="m-0">
+                帝国的危机已经危在旦夕，我们必须倾尽全力，我们必须速战速决，我们别无选择。
+            </p>
+        </template>
+    </Card>
+</template>
+
+<style scoped></style>
